506ca4d3053585d7742ed7b8e683ca3e70145184
MuhammadRamdani121 <muhamadramdani121.mr60@gmail.com>
2026-06-14 21:51:00 +0700

    Menambahkan Props

--- src/components/minggu_1/Props_2.jsx
+++ src/components/minggu_1/Props_2.jsx
@@ -1,11 +1,17 @@
 export default function Props_2() {
   return (
     <div>
-      <h1 className="text-lg font-bold">Ini Adalah JSX</h1>
-      <span>JSX Code Html Bisa Masukasdasdsad</span>
-      <pre>
-        <code className="text-xl font-bold">
-          {`
+      <h1 className="text-lg font-bold">Component</h1>
+      <span>Memecah UI Menjadi Bagian Kecil</span>
+
+      <div className="flex flex-col gap-10 bg-red-100">
+        <button className="border-2 bg-black p-2 text-white">
+          Ini Contoh button
+        </button>
+
+        <pre className="">
+          <code className="text-xl font-bold">
+            {`
             export default function JSX(){
             return(
             <>
@@ -13,8 +19,9 @@ export default function Props_2() {
             </>
             )}
             `}
-        </code>
-      </pre>
+          </code>
+        </pre>
+      </div>
     </div>
   );
 }
