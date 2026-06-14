9c0f2015afaf3becbc2343de41e28f823cdead11
MuhammadRamdani121 <muhamadramdani121.mr60@gmail.com>
2026-06-15 01:13:04 +0700

    Menambahkan Event Klik

---
+++ src/components/minggu_1/Event_4.jsx
@@ -0,0 +1,31 @@
+export default function Event_4() {
+  return (
+    <div>
+      <h1>Event Klik</h1>
+      <span>Suatu Aturan Ketika Di Klik</span>
+
+      <div>
+        <pre>
+          <code>
+            {`
+                export default function handleClick(){
+                alert("Hallo")
+                return(
+                
+                <div>
+                <button onClick={handleClick}>
+                Klik
+                </button>
+                </div>
+                
+                
+                )}
+                
+                
+                `}
+          </code>
+        </pre>
+      </div>
+    </div>
+  );
+}
