c85340ee6c7f0d0699ec66fa4708378129e63bbb
MuhammadRamdani121 <muhamadramdani121.mr60@gmail.com>
2026-06-15 22:56:54 +0700

    Memperbarui Laithan 1

--- src/components/minggu_1/Latihan_1/Latihan_1.jsx
+++ src/components/minggu_1/Latihan_1/Latihan_1.jsx
@@ -3,11 +3,22 @@ import User from "../Latihan_1/User";
 export default function Latihan_1() {
   return (
     <div>
-      <h1>Latihan 1</h1>
+      <header>
+        <h1 className="text-center text-2xl font-bold">
+          Latihan Minggu 1
+        </h1>
+      </header>
 
-      <div>
-        <User nama="Ramdani" kelas="12 IPA" />
-      </div>
+      <main className="mt-10 grid grid-cols-2 px-6">
+        <section>
+          <div>
+            <User nama="Ramdani" kelas="12 IPA" />
+          </div>
+          <div>
+            <User nama="Ramdani" kelas="12 IPA" />
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
