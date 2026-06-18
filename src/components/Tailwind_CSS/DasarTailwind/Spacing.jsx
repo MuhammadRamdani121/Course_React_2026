a831e1d01b99a5574942a1ffda015363b24d2431
MuhammadRamdani121 <muhamadramdani121.mr60@gmail.com>
2026-06-18 22:18:26 +0700

    Memperbarui Spacing

--- src/components/Tailwind_CSS/DasarTailwind/Spacing.jsx
+++ src/components/Tailwind_CSS/DasarTailwind/Spacing.jsx
@@ -4,43 +4,67 @@ export default function Spacing() {
       <main>
         <h1 className="text-center text-4xl">Spacing</h1>
 
-        <section className="border text-center">
-          <aside className="grid grid-cols-3 items-center justify-center">
-            <article className="grid grid-cols-1 items-center justify-center">
-              <h1 className="bg-red-200 bg-top-left">
-                Pading
-              </h1>
-              <h1>Pading X/Horizontal</h1>
-              <h1>Pading Y/Vertikal</h1>
-              <h1>Margin</h1>
-              <h1>Margin X/Horizontal</h1>
-              <h1>Margin Y/Vertikal</h1>
-            </article>
+        <section className="grid grid-cols-3 items-center justify-center gap-1 border">
+          <article className="grid items-center justify-center gap-8 border text-center">
+            <h1>Padding</h1>
+            <h1>Padding Horizontal (X)</h1>
+            <h1>Padding Vertikal (Y)</h1>
+            <h1>Margin</h1>
+            <h1>Margin Horizontal (X)</h1>
+            <h1>Margin Vertikal (Y)</h1>
+          </article>
 
-            <article className="w-/2 flex flex-col items-center justify-center gap-2">
+          <article className="grid gap-2 border border-black text-white">
+            <div className="border">
               <h1 className="bg-black p-4 text-white">
-                Contoh Pading Semua Arah
+                Padding p*4
               </h1>
+            </div>
+            <div className="border">
               <h1 className="bg-black px-4 text-white">
-                Contoh Padding Horizontal (Width)
+                Padding Horizontal (X) px*4
               </h1>
+            </div>
+            <div className="border">
               <h1 className="bg-black py-4 text-white">
-                Contoh Padding Vertikal (Height)
+                Padding Vertical (Y) py*4
               </h1>
-              <div className="border">
-                <h1 className="m-4 bg-black text-white">
-                  Contoh Margin Semua Arah
-                </h1>
-              </div>
+            </div>
 
-              <h1>Margin X/Horizontal</h1>
-              <h1>Margin Y/Vertikal</h1>
-            </article>
-          </aside>
+            <div className="border border-black bg-white">
+              <h1 className="m-4 bg-black">Margin m*4</h1>
+            </div>
+            <div className="border border-black bg-white">
+              <h1 className="mx-4 bg-black">
+                Margin Horizontal (X) mx*4
+              </h1>
+            </div>
+            <div className="border border-black bg-white">
+              <h1 className="my-4 bg-black">
+                Margin Vertikal (Y) my*4
+              </h1>
+            </div>
+            {/*  */}
+          </article>
 
-          <article className="">
+          <article className="grid items-center justify-center gap-8 border">
             <pre>
-              <code>{}</code>
+              <code>{`<h1 className="p-4 "></h1>`}</code>
+            </pre>
+            <pre>
+              <code>{`<h1 className="pX-4 "></h1>`}</code>
+            </pre>
+            <pre>
+              <code>{`<h1 className="pY-4 "></h1>`}</code>
+            </pre>
+            <pre>
+              <code>{`<h1 className="m-4 "></h1>`}</code>
+            </pre>
+            <pre>
+              <code>{`<h1 className="mx-4 "></h1>`}</code>
+            </pre>
+            <pre>
+              <code>{`<h1 className="my-4 "></h1>`}</code>
             </pre>
           </article>
         </section>
