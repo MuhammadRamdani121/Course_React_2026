0a15618c5fb913b93d27a2b3a9691b654c4ef136
MuhammadRamdani121 <muhamadramdani121.mr60@gmail.com>
2026-06-18 22:22:05 +0700

    Memperbarui Width And Hight

--- src/components/Tailwind_CSS/DasarTailwind/WidthHight.jsx
+++ src/components/Tailwind_CSS/DasarTailwind/WidthHight.jsx
@@ -2,6 +2,19 @@ export default function WidthHight() {
   return (
     <div>
       <h1>Width And Hight</h1>
+
+      <main>
+        <section className="grid grid-cols-2 items-center justify-center">
+          <article>
+            <h1></h1>
+          </article>
+          <article>
+            <pre>
+              <code></code>
+            </pre>
+          </article>
+        </section>
+      </main>
     </div>
   );
 }
