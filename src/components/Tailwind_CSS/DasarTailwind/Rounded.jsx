f9c8d3b1587a23f1cd3c0691c1b7efbd28f10bb7
MuhammadRamdani121 <muhamadramdani121.mr60@gmail.com>
2026-06-18 23:50:23 +0700

    Memperbarui Rounded

--- src/components/Tailwind_CSS/DasarTailwind/Rounded.jsx
+++ src/components/Tailwind_CSS/DasarTailwind/Rounded.jsx
@@ -1,7 +1,15 @@
 export default function Rounded() {
   return (
     <div>
-      <h1></h1>
+      <h1>Rounded</h1>
+      <main>
+        <section>
+          <article>
+            <h1></h1>
+          </article>
+          <article></article>
+        </section>
+      </main>
     </div>
   );
 }
