8a3af99444fc0b189625e4b8c0212850a4e9070b
MuhammadRamdani121 <muhamadramdani121.mr60@gmail.com>
2026-06-18 23:12:09 +0700

    Membuat Width Hight

--- src/components/Tailwind_CSS/DasarTailwind/WidthHight.jsx
+++ src/components/Tailwind_CSS/DasarTailwind/WidthHight.jsx
@@ -6,11 +6,23 @@ export default function WidthHight() {
       <main>
         <section className="grid grid-cols-2 items-center justify-center">
           <article>
-            <h1></h1>
+            <h1 className="w-64">Width w*64</h1>
+            <h1 className="w-full">Width Full</h1>
+            <h1 className="w-screen">Width Screen</h1>
+
+            <h1 className="h-64">Height h*64</h1>
+            <h1 className="h-full">Height full</h1>
+            <h1 className="h-screen">Heihgt Screen</h1>
           </article>
           <article>
             <pre>
-              <code></code>
+              <code>{`<h1 className="w-64">Width w*64</h1>`}</code>
+              <code>{`<h1 className="w-full">Width Full</h1>`}</code>
+              <code>{`<h1 className="w-screen">Width Screen</h1>`}</code>
+
+              <code>{`<h1 className="h-64">Height h*64</h1>`}</code>
+              <code>{`<h1 className="h-full">Height full</h1>`}</code>
+              <code>{`<h1 className="h-screen">Heihgt Screen</h1>`}</code>
             </pre>
           </article>
         </section>
