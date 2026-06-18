8dff119ca5bc758232ea139f3575550d6c1c7b25
MuhammadRamdani121 <muhamadramdani121.mr60@gmail.com>
2026-06-18 23:32:11 +0700

    Menambahkan Border

---
+++ src/components/Tailwind_CSS/DasarTailwind/Border.jsx
@@ -0,0 +1,13 @@
+export default function Border() {
+  return (
+    <div>
+      <h1>Border</h1>
+      <main>
+        <section>
+          <article></article>
+          <aside></aside>
+        </section>
+      </main>
+    </div>
+  );
+}
