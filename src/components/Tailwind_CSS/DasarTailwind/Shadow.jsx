134cb9a79dfd4a9e86b9572270b28c0c1c0fa193
MuhammadRamdani121 <muhamadramdani121.mr60@gmail.com>
2026-06-19 23:07:09 +0700

    Memperbarui Shadow

---
+++ src/components/Tailwind_CSS/DasarTailwind/Shadow.jsx
@@ -0,0 +1,14 @@
+export default function Shadow() {
+  return (
+    <div>
+      <h1>Shadow</h1>
+
+      <main>
+        <section>
+          <article>shadow-sm</article>
+        </section>
+        <section></section>
+      </main>
+    </div>
+  );
+}
